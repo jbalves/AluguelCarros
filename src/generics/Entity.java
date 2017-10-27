225b0d9ec0d0bc085e50fb89098328254b89f996
Jeferson Barros Alves <jeferson.ba@sidiamao.com.br>
2017-10-27 12:09:50 -0400

    Create Entity class

---
+++ src/generics/Entity.java
@@ -0,0 +1,17 @@
+package generics;
+
+public abstract class Entity{
+	private String id;
+
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+
+}
