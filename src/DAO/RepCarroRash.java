572e74056d4d693ef15be109845695fd823e7410
Jeferson Barros Alves <jeferson.ba@sidiamao.com.br>
2017-10-27 14:03:48 -0400

    Initial RepCaroRash

---
+++ src/DAO/RepCarroRash.java
@@ -0,0 +1,32 @@
+package DAO;
+
+import interfaces.IRepCarro;
+import model.Carro;
+
+public class RepCarroRash implements IRepCarro {
+
+	@Override
+	public void inserir(Carro c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Carro consultar(String cod) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remover(Carro e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Carro e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
